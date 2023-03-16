c91378339321f6ca9cb04475b27e752399e9f7e9
Elson Tan <elsodev.et@gmail.com>
2023-03-16 15:20:44 +0800

    FIX test only unit folder

---
+++ tests/Feature/FrontendTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FrontendTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testSupposeFrontendRedirectToAdmin()
+    {
+        $response = $this->get('/'); 
+
+        $response->assertStatus(302);
+    }
+}
