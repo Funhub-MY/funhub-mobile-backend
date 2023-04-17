b4102b72ffa84e638facd9e97c5c9c9d25e51081
Elson Tan <elsodev.et@gmail.com>
2023-04-17 14:43:58 +0800

    FIX #18

--- app/Http/Controllers/Api/ShareableLinkController.php
+++ app/Http/Controllers/Api/ShareableLinkController.php
@@ -42,20 +42,33 @@ class ShareableLinkController extends Controller
             // detect if ios redirect to app store, else redirec to play store
             if (preg_match('/(ios)/i', $userAgent)) {
                 // eg. flutter://flutter.dev?article_id=1
+                Log::info('Redirecting to ios deep link', [
+                    'link' => config('app.ios_deep_link').'?'.$linkStructure,
+                ]);
                 return redirect(config('app.ios_deep_link').'?'.$linkStructure);
             } else {
+                // eg. flutter://flutter.dev?article_id=1
+                Log::info('Redirecting to android deep link', [
+                    'link' => config('app.android_deep_link').'?'.$linkStructure,
+                ]);
                 return redirect(config('app.android_deep_link').'?'.$linkStructure);
             }
         } else {
             // detect if ios redirect to app store, else redirec to play store
             if (preg_match('/(ios)/i', $userAgent)) {
+                Log::info('Redirecting to ios app store', [
+                    'link' => config('app.ios_app_store_link'),
+                ]);
                 return redirect(config('app.ios_app_store_link'));
             } else {
+                Log::info('Redirecting to android play store', [
+                    'link' => config('app.android_play_store_link'),
+                ]);
                 return redirect(config('app.android_play_store_link'));
             }
         }
 
-        return redirect($link);
+        return abort(404);
     }
 
     /**
