ff919b23f0bade1a1769915c0a826e5033d0ab50
Elson Tan <elsodev.et@gmail.com>
2023-08-10 13:49:29 +0800

    FIX

--- app/Services/SecureHash.php
+++ app/Services/SecureHash.php
@@ -5,43 +5,23 @@ use Exception;
 use Illuminate\Support\Facades\Log;
 
 class SecureHash {
-    // This is an array for creating hex chars
-    const HEX_TABLE = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F');
 
-    /**
-     * This method performs an arrangement with the byte array data, convert it into hexadecimal format and returns the final string.
-     *
-     * @param $input - the byte array of the initial string
-     * @return string - the string for the resulting hash value
-     */
-    static function hex($input) {
-        // create a StringBuffer 2x the size of the hash array
-        $sb = '';
-        // retrieve the byte array data, convert it to hex and add it to the StringBuffer
-        for ($i = 0; $i < count($input); $i++) {
-            $sb .= self::HEX_TABLE[($input[$i] >> 4) & 0xf];
-            $sb .= self::HEX_TABLE[$input[$i] & 0xf];
-        }
-        return $sb;
+    public static function hex($input) {
+      
+      $hex_table = array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F');
+      
+      $output = "";
+      
+      foreach($input as $byte) {
+        $output .= $hex_table[($byte >> 4) & 0x0F];
+        $output .= $hex_table[$byte & 0x0F]; 
+      }
+      
+      return $output;
     }
-
-    /**
-     * This method will return a string of hash value using the original.
-     *
-     * @param $originalString - the original string that will be used to produce the final hash value.
-     * @return string - the string for the resulting hash value
-     */
+    
     public static function generateSecureHash($originalString) {
-        $md = null;
-        $ba = null;
-        
-        // create the md hash and ISO-8859-1 encode it
-        try {
-            $md = hash('sha256', $originalString, true);
-            $ba = array_values(unpack('C*', $md));
-        } catch (Exception $e) {
-            // won't happen
-        }
-        return self::hex($ba);
+      $hash = hash('sha256', $originalString); 
+      return self::hex(str_split($hash));
     }
-}
+  }
