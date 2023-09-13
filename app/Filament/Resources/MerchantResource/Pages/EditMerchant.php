05a074a069ef476b38d701dfeebda6daa6eefd9c
Elson Tan <elsodev.et@gmail.com>
2023-09-13 18:14:24 +0800

    ADHOC FIX: Merchant redeem code is null

--- app/Filament/Resources/MerchantResource/Pages/EditMerchant.php
+++ app/Filament/Resources/MerchantResource/Pages/EditMerchant.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\MerchantResource\Pages;
 
 use App\Filament\Resources\MerchantResource;
+use App\Models\Merchant;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -15,5 +16,19 @@ class EditMerchant extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (!$data['redeem_code'] || $data['redeem_code'] == '') { // if no redeem_code
+            // ensure redeem code is unique loop
+            $maxTries = 0;
+            $data['redeem_code'] = rand(100000, 999999);
+            while (Merchant::where('redeem_code', $data['redeem_code'])->exists() && $maxTries < 10) {
+                $data['redeem_code'] = rand(100000, 999999);
+                $maxTries++;
+            }
+        }
+        return $data;
     }
 }
