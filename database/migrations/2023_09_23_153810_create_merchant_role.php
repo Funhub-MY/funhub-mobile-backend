32a1fca22e2d6212533e2d4f7ed98d1beb0aaffd
Elson Tan <elsodev.et@gmail.com>
2023-09-25 22:47:34 +0800

    FIX pipeline

--- database/migrations/2023_09_23_153810_create_merchant_role.php
+++ database/migrations/2023_09_23_153810_create_merchant_role.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Spatie\Permission\Contracts\Permission;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 return new class extends Migration
