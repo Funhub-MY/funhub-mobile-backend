d87402413ca9b64391824c250bc7958653ca327e
Elson Tan <elsodev.et@gmail.com>
2023-09-28 13:28:24 +0800

    DISABLE filters first

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -43,7 +43,7 @@ class Dashboard extends BasePage
         if (auth()->user()->hasRole('merchant')) {
             // load filers and merchant releated widgets
             $widgets = array_merge($widgets, [
-                Filters::class,
+                // Filters::class,
                 VoucherAvailable::class,
                 RedemptionsOverview::class,
                 RedemptionsValue::class,
