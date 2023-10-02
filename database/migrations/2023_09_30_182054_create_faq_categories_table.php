103fc7846db052945d2189d0fa41d8ddded652d2
Elson Tan <elsodev.et@gmail.com>
2023-10-02 10:27:00 +0800

    Help center initial

---
+++ database/migrations/2023_09_30_182054_create_faq_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faq_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('is_featured')->default(false);
+            $table->string('lang')->default('cn');
+            $table->foreignId('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faq_categories');
+    }
+};
