7232190b0ed4d6298adcabe88256e9a6ae289573
Elson Tan <elsodev.et@gmail.com>
2023-10-03 12:25:49 +0800

    ADDED published scope to faq api

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -24,5 +25,11 @@ class Faq extends Model
     public function scopeFeatured($query)
     {
         return $query->where('is_featured', true);
+    }
+
+
+    public function scopePublished(Builder $query) : void
+    {
+        $query->where('status', 1);
     }
 }
