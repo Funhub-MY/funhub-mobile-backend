8f35f53bfbd32b8d1b743c29e49e7109ee9a5a2d
Elson Tan <elsodev.et@gmail.com>
2023-10-03 16:12:32 +0800

    FIX

--- app/Http/Resources/FaqResource.php
+++ app/Http/Resources/FaqResource.php
@@ -24,6 +24,6 @@ class FaqResource extends JsonResource
             ],
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-        ]
+        ];
     }
 }
