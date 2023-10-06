70e1a045575c77ad3bb20914dca48bbd9f01c604
Elson Tan <elsodev.et@gmail.com>
2023-10-06 12:54:36 +0800

    ADDED icon to faq category

--- app/Models/FaqCategory.php
+++ app/Models/FaqCategory.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class FaqCategory extends Model
+class FaqCategory extends Model implements InteractsWithMedia
 {
-    use HasFactory;
+    use HasFactory, HasMedia;
 
     protected $guarded = ['id'];
 
