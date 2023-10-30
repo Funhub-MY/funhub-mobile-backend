eadfb69226fad30f2e8ce6f4bea4d5aa54a9c98e
Elson Tan <elsodev.et@gmail.com>
2023-10-30 14:58:24 +0800

    ENHANCED nearby query

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Netsells\GeoScope\Traits\GeoScopeTrait;
@@ -100,5 +101,95 @@ class Location extends Model implements HasMedia
     public function getFullAddressAttribute(): string
     {
         return $this->address . ', ' . $this->city . ', ' . $this->state->name . ', ' . $this->country->name;
+    }
+
+    /**
+     * Scope a query to only include locations within a given distance.
+     */
+    public function scopeWithinKmOf($query, $latitude, $longitude, $km)
+    {
+        $box = static::boundingBox($latitude, $longitude, $km);
+
+        Log::info('box', [
+            'box' => $box,
+            'lat' => $latitude,
+            'lng' => $longitude,
+            'km' => $km,
+        ]);
+
+        $query
+            // Latitude part of the bounding box.
+            ->whereBetween('lat', [
+                $box['minLat'],
+                $box['maxLat']
+            ])
+            // Longitude part of the bounding box.
+            ->whereBetween('lng', [
+                $box['minLon'],
+                $box['maxLon']
+            ])
+            // Accurate calculation that eliminates false positives.
+            ->whereRaw('(ST_Distance_Sphere(point(lng, lat), point(?,?))) <= ?', [
+                $longitude,
+                $latitude,
+                $km
+            ]);
+    }
+
+    /**
+     * Get the bounding box of a location.
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @param float $distance
+     * @return float
+     */
+    public static function boundingBox($latitude, $longitude, $distance)
+    {
+        $latLimits = [deg2rad(-90), deg2rad(90)];
+        $lonLimits = [deg2rad(-180), deg2rad(180)];
+
+        $radLat = deg2rad($latitude);
+        $radLon = deg2rad($longitude);
+
+        if ($radLat < $latLimits[0] || $radLat > $latLimits[1]
+            || $radLon < $lonLimits[0] || $radLon > $lonLimits[1]) {
+            throw new \Exception("Invalid Argument");
+        }
+
+        // Angular distance in radians on a great circle,
+        // using Earth's radius in km.
+        $angular = $distance / 6371;
+
+        $minLat = $radLat - $angular;
+        $maxLat = $radLat + $angular;
+
+        if ($minLat > $latLimits[0] && $maxLat < $latLimits[1]) {
+            $deltaLon = asin(sin($angular) / cos($radLat));
+            $minLon = $radLon - $deltaLon;
+
+            if ($minLon < $lonLimits[0]) {
+                $minLon += 2 * pi();
+            }
+
+            $maxLon = $radLon + $deltaLon;
+
+            if ($maxLon > $lonLimits[1]) {
+                $maxLon -= 2 * pi();
+            }
+        } else {
+            // A pole is contained within the distance.
+            $minLat = max($minLat, $latLimits[0]);
+            $maxLat = min($maxLat, $latLimits[1]);
+            $minLon = $lonLimits[0];
+            $maxLon = $lonLimits[1];
+        }
+
+        return [
+            'minLat' => rad2deg($minLat),
+            'minLon' => rad2deg($minLon),
+            'maxLat' => rad2deg($maxLat),
+            'maxLon' => rad2deg($maxLon),
+        ];
     }
 }
