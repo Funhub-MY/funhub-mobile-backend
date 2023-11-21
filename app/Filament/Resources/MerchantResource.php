47064d39a211525d99d502d92fc79e72aa69ec76
Veronica <veronica.lim@nedex.io>
2023-11-21 11:54:08 +0800

    Fix: merchant create error due $record null

--- app/Filament/Resources/MerchantResource.php
+++ app/Filament/Resources/MerchantResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources;
 
+use Closure;
 use Filament\Forms;
 use App\Models\User;
 use Filament\Tables;
@@ -13,6 +14,8 @@ use Filament\Resources\Resource;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Filament\Tables\Actions\BulkAction;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\Component;
 use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
 use Illuminate\Database\Eloquent\Builder;
@@ -22,8 +25,7 @@ use App\Filament\Resources\MerchantResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use App\Filament\Resources\MerchantResource\RelationManagers;
-use Closure;
-use Illuminate\Database\Eloquent\Model;
+use SebastianBergmann\Type\NullType;
 
 class MerchantResource extends Resource
 {
@@ -61,12 +63,18 @@ class MerchantResource extends Resource
                             ->email(true)
                             ->helperText('System auto send an email with their Login Email, Password to this address when created.')
                             ->required()
-                            ->rules(['email', 'required', function ($context, Model $record) {
-                                return function (string $attribute, $value, Closure $fail) use ($record) {
-                                    $is_user_exists = User::where('email', $value) // check if email already existed in the User table, 
-                                        ->where('id', '!=', $record->user_id) // excluding current user record in the table
-                                        ->exists();
-
+                            ->rules(['email', 'required', function ($context, ?Model $record) {
+                                return function (string $attribute, $value, Closure $fail) use ($context, $record) {
+                                    if ($context === 'create' || !$record) {
+                                        $is_user_exists = User::where('email', $value) // check if email already existed in the User table, 
+                                            ->exists();
+                                    } elseif ($context === 'edit' && $record instanceof Model)  {
+                                        $is_user_exists = User::where('email', $value) // check if email already existed in the User table, 
+                                            ->where('id', '!=', $record->user_id) // excluding current user record in the table
+                                            ->exists();
+                                    }
+                                    
+                                    // Check the result and fail if the email already exists
                                     if ($is_user_exists) {
                                         $fail('The :attribute is exists');
                                     }
