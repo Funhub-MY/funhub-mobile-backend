7b23b3c7b67f949a03985c44e1289588dec90812
Elson Tan <elsodev.et@gmail.com>
2023-12-01 10:26:41 +0800

    ADHOC FIX

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -617,7 +617,7 @@ class AuthController extends Controller
 
 
         // latest provider id if providerdata > 1
-        $providerId = (count($firebase_user->providerData) > 0) ? $firebase_user->providerData[0]->providerId : $firebase_user->providerData[count($firebase_user->providerData) - 1]->providerId;
+        $providerId = (count($firebase_user->providerData) > 1) ? $firebase_user->providerData[count($firebase_user->providerData) - 1]->providerId : $firebase_user->providerData[0]->providerId;
         $name = null;
         if ($firebase_user->displayName == null || $firebase_user->displayName == '') {
             $pattern = '/(.*)@.*$/';
