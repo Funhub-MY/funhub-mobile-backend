bd7b509078505ce9a8e3c02d07952472194db89b
Elson Tan <elsodev.et@gmail.com>
2023-12-14 12:26:34 +0800

    FIXES

--- app/Filament/Resources/CampaignQuestionResource/Pages/CreateCampaignQuestion.php
+++ app/Filament/Resources/CampaignQuestionResource/Pages/CreateCampaignQuestion.php
@@ -13,11 +13,12 @@ class CreateCampaignQuestion extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        dd($data);
         if ($data['answer_type'] != 'text') {
-            $data['answer'] = json_encode($data['answers']);
+            // map answers = ['answer' => 1] to jus [1, 2, 3]
+            $data['answer'] = json_encode(array_map(function ($answer) {
+                return $answer['answer'];
+            }, $data['answers']));
         }
-
         return $data;
     }
 }
--- app/Filament/Resources/CampaignQuestionResource/Pages/EditCampaignQuestion.php
+++ app/Filament/Resources/CampaignQuestionResource/Pages/EditCampaignQuestion.php
@@ -16,4 +16,30 @@ class EditCampaignQuestion extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if ($data['answer_type'] != 'text') {
+            // map answers = ['answer' => 1] to jus [1, 2, 3]
+            $data['answer'] = json_encode(array_map(function ($answer) {
+                return $answer['answer'];
+            }, $data['answers']));
+        }
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        // if answer_type is not text, then convert answer json to ['answer' => 1]
+        if ($data['answer_type'] != 'text') {
+            if ($data['answer']) {
+                $data['answers'] = array_map(function ($answer) {
+                    return ['answer' => $answer];
+                }, json_decode($data['answer']));
+            }
+        }
+
+        return $data;
+    }
 }
