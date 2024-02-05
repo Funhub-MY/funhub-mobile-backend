f8605e0df366cd2c0f9480e824209cb59c1d67e6
Elson Tan <elsodev.et@gmail.com>
2024-02-05 12:02:26 +0800

    FIX

--- app/Console/Commands/ProcessArticleToCategoriesML.php
+++ app/Console/Commands/ProcessArticleToCategoriesML.php
@@ -53,58 +53,65 @@ class ProcessArticleToCategoriesML extends Command
         $totalTokens = 0;
 
         foreach ($articles as $article) {
-            // sleep for every 5 requests
-            if ($totalArticlesProcessed > 0 && $totalArticlesProcessed % 5 == 0) {
-                $this->info('Sleeping for 20 seconds avoid tpm');
-                sleep(20);
-            }
-
-            $result = $client->chat()->create([
-                'model' => 'gpt-3.5-turbo-0613',
-                'messages' => [
-                    ['role' => 'user', 'content' => 'Categorise content into provided (must use) category list: '. $subCategories->pluck('name')->implode(', '). ' must use and return categories names only separated by comma without space,content: '. $this->generateContent($article)],
-                ],
-            ]);
-
-            // $this->info('Content: ' . $this->generateContent($article));
-
-            $this->info('Article ID:' . $article->id .', Result: '. $result->choices[0]->message->content. ' | Total Tokens used: '. $result->usage->totalTokens );
-            Log::info('[ProcessArticleCategories] Article ID:' . $article->id .', Result: '. $result->choices[0]->message->content. ' | Total Tokens used: '. $result->usage->totalTokens );
-
-            $totalTokens += $result->usage->totalTokens;
-
-            $resultCategories = explode(',', $result->choices[0]->message->content);
-            foreach ($resultCategories as $resultCategory) {
-                $resultCategory = trim($resultCategory);
-                $subCat = $subCategories->where('name', $resultCategory)->first();
-                if ($subCat) {
-                    // attach if dosent exist
-                    if ($article->subCategories->where('id', $subCat->id)->count() == 0) {
-                        $article->subCategories()->attach($subCat->id);
-                        // refresh article subCategories
-                        $article->refresh();
-
-                        $this->info('--- Article ID:' . $article->id .', Attached Sub Category: '. $subCat->name);
-                        Log::info('[ProcessArticleCategories] Article ID:' . $article->id .', Attached Sub Category: '. $subCat->name);
-                        $totalSubcategoriesAttached++;
-                    }
-
-                    // attach parent
-                    if ($article->categories->where('id', $subCat->parent_id)->count() == 0) {
-                        $article->categories()->attach($subCat->parent_id);
-                        // refresh article categories
-                        $article->refresh();
-                        $this->info('--- Article ID:' . $article->id .', Attached Parent Category: '. $subCat->parent->name);
-                        Log::info('[ProcessArticleCategories] Article ID:' . $article->id .', Attached Parent Category: '. $subCat->parent->name);
-                        $totalParentCategoriesAttached++;
-                    }
-                } else {
-                    // category not found
-                    $this->info('--- Article ID:' . $article->id .', Category not found: '. $resultCategory);
+            try {
+                // sleep for every 5 requests
+                if ($totalArticlesProcessed > 0 && $totalArticlesProcessed % 5 == 0) {
+                    $this->info('Sleeping for 20 seconds avoid tpm');
+                    sleep(20);
                 }
-            }
 
-            $totalArticlesProcessed++;
+                $result = $client->chat()->create([
+                    'model' => 'gpt-3.5-turbo-0613',
+                    'messages' => [
+                        ['role' => 'user', 'content' => 'Categorise content into provided (must use) category list: '. $subCategories->pluck('name')->implode(', '). ' must use and return categories names only separated by comma without space,content: '. $this->generateContent($article)],
+                    ],
+                ]);
+
+                // $this->info('Content: ' . $this->generateContent($article));
+
+                $this->info('Article ID:' . $article->id .', Result: '. $result->choices[0]->message->content. ' | Total Tokens used: '. $result->usage->totalTokens );
+                Log::info('[ProcessArticleCategories] Article ID:' . $article->id .', Result: '. $result->choices[0]->message->content. ' | Total Tokens used: '. $result->usage->totalTokens );
+
+                $totalTokens += $result->usage->totalTokens;
+
+                $resultCategories = explode(',', $result->choices[0]->message->content);
+                foreach ($resultCategories as $resultCategory) {
+                    $resultCategory = trim($resultCategory);
+                    $subCat = $subCategories->where('name', $resultCategory)->first();
+                    if ($subCat) {
+                        // attach if dosent exist
+                        if ($article->subCategories->where('id', $subCat->id)->count() == 0) {
+                            $article->subCategories()->attach($subCat->id);
+                            // refresh article subCategories
+                            $article->refresh();
+
+                            $this->info('--- Article ID:' . $article->id .', Attached Sub Category: '. $subCat->name);
+                            Log::info('[ProcessArticleCategories] Article ID:' . $article->id .', Attached Sub Category: '. $subCat->name);
+                            $totalSubcategoriesAttached++;
+                        }
+
+                        // attach parent
+                        if ($article->categories->where('id', $subCat->parent_id)->count() == 0) {
+                            $article->categories()->attach($subCat->parent_id);
+                            // refresh article categories
+                            $article->refresh();
+                            $this->info('--- Article ID:' . $article->id .', Attached Parent Category: '. $subCat->parent->name);
+                            Log::info('[ProcessArticleCategories] Article ID:' . $article->id .', Attached Parent Category: '. $subCat->parent->name);
+                            $totalParentCategoriesAttached++;
+                        }
+                    } else {
+                        // category not found
+                        $this->info('--- Article ID:' . $article->id .', Category not found: '. $resultCategory);
+                    }
+                }
+
+                $totalArticlesProcessed++;
+            } catch (\Exception $e) {
+                Log::error('Error processing article: '. $article->id);
+                $this->error('Error processing article, skipping: '. $article->id);
+                Log::error($e->getMessage());
+                continue;
+            }
         }
 
         $this->info('Total Articles Processed: '. $totalArticlesProcessed);
