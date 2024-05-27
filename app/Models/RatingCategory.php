a6bdec43725c701f394fa9cb2cda970315bfa703
Elson Tan <elsodev.et@gmail.com>
2024-05-27 12:57:17 +0800

    FIX

--- app/Models/RatingCategory.php
+++ app/Models/RatingCategory.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class RatingCategory extends BaseModel
+class RatingCategory extends Model
 {
     use HasFactory;
 
