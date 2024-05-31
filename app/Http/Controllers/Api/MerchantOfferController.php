59812ecc9bd6fe480c763f0a0feaa087a813e28e
Elson Tan <elsodev.et@gmail.com>
2024-05-31 18:28:38 +0800

    ADHOC PROD FIX Merchant offer should be visible in article

--- app/Http/Controllers/Api/MerchantOfferController.php
+++ app/Http/Controllers/Api/MerchantOfferController.php
@@ -150,12 +150,21 @@ class MerchantOfferController extends Controller
             });
         }
 
+
         // location id
         if ($request->has('location_id')) {
-            $query->whereHas('location', function ($query) use ($request) {
-                $query->where('locations.id', $request->location_id);
+            // where two condition merchant offer locaiton tagged same location id or merchant offer's stores tagged same location id
+            $query->where(function ($subQuery) {
+                $subQuery->whereHas('location', function ($query) {
+                    $query->where('locations.id', request()->location_id);
+                })->orWhereHas('stores', function ($query) {
+                    $query->whereHas('location', function ($query) {
+                        $query->where('locations.id', request()->location_id);
+                    });
+                });
             });
         }
+
 
         if ($request->has('store_id')) {
             // explode store_id if has comma
