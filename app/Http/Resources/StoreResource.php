acef75f35a8536f46390385a2994131c0a0e26ef
Elson Tan <elsodev.et@gmail.com>
2024-06-24 13:13:57 +0800

    FIX

--- app/Http/Resources/StoreResource.php
+++ app/Http/Resources/StoreResource.php
@@ -105,7 +105,7 @@ class StoreResource extends JsonResource
             'ratings' => number_format(floatval($this->ratings), 1),
             'total_ratings' => $this->store_ratings_count + $this->location_ratings_count,
             'total_article_ratings' => $this->location_ratings_count,
-            'total_articles_same_location' => $this->articles_count,
+            'total_articles_same_location' => $this->articles->count(),
             'followings_been_here' => $this->whenLoaded('articles', function () {
                 $uniqueUsers = $this->articles->map(function ($article) {
                     $user = $article->user;
