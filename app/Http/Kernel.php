325789986b01c1f19b8c2e4cdbf3ce64cc7a60f2
Elson Tan <elsodev.et@gmail.com>
2024-07-05 11:58:12 +0800

    REVRT normal rate limit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:720,60',  // 720 requests per minute (12 per second)
+            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
