a36013ba2ed95468daece764bc2d720821ad52a1
Elson Tan <elsodev.et@gmail.com>
2024-07-15 13:05:23 +0800

    FIX

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -94,7 +94,7 @@ class Article extends BaseModel implements HasMedia, Auditable
             // 'thumbnail' => $this->getFirstMediaUrl(self::MEDIA_COLLECTION_NAME),
             'type' => $this->type,
             // remove all html tags and new lines and limit to 6000 characters
-            'body' => substr(preg_replace('/\s+/', ' ', trim(strip_tags($this->body))), 0, 6000),
+            'body' => substr(preg_replace('/\s+/', ' ', trim($this->body)), 0, 6000),
             'categories' => $this->categories->pluck('name', 'name_translation'),
             'tags' => $this->tags->pluck('name'),
             'status' => $this->status,
