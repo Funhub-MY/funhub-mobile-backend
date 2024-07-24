2d0c8c3c56327fedc7aaead4d31f3e4e106a7760
Elson Tan <elsodev.et@gmail.com>
2024-07-24 21:04:25 +0800

    FIX bubble data sync

--- app/Console/Commands/PullBubbleDataForUserStoreRatings.php
+++ app/Console/Commands/PullBubbleDataForUserStoreRatings.php
@@ -69,165 +69,163 @@ class PullBubbleDataForUserStoreRatings extends Command
 
         $reviewsData = $reviewsData['response']['results'];
 
-        // log how m,any data got from bubble into Log
-        Log::info('Bubble API Response Data Pull', [
-            'users' => count($usersData),
-            'reviews' => count($reviewsData),
-            'stores' => count($storesData),
-        ]);
-        // Get unique users by phone number
-        $uniqueUsers = $this->getUniqueUsersByPhoneNumber($usersData);
+        // // Get unique users by phone number
+        // $uniqueUsers = $this->getUniqueUsersByPhoneNumber($usersData);
 
-        // Process reviews and link them to users and stores
-        if (is_array($reviewsData)) {
-            foreach ($reviewsData as $review) {
-                if (isset($review['Created By']) && isset($review['Store'])) {
+        // // Process reviews and link them to users and stores
+        // if (is_array($reviewsData)) {
+        //     foreach ($reviewsData as $review) {
+        //         if (isset($review['Created By']) && isset($review['Store'])) {
 
-                    $userId = $review['Created By'];
-                    $storeId = $review['Store'];
-                    $store = $this->getStoreById($storesData, $storeId);
+        //             $userId = $review['Created By'];
+        //             $storeId = $review['Store'];
+        //             $store = $this->getStoreById($storesData, $storeId);
 
-                    if ($store && isset($store['funhub_store_id'])) {
-                        $funhubStoreId = $store['funhub_store_id'];
-                        $rating = $review['Rating'] ?? null;
-                        $categories = $review['Topic'] ?? [];
-                        $createdAt = $review['Created Date'] ?? null;
+        //             if ($store && isset($store['funhub_store_id'])) {
+        //                 $funhubStoreId = $store['funhub_store_id'];
+        //                 $rating = $review['Rating'] ?? null;
+        //                 $categories = $review['Topic'] ?? [];
+        //                 $createdAt = $review['Created Date'] ?? null;
 
-                        // Find the user by ID
-                        foreach ($uniqueUsers as &$user) {
-                            if (isset($user['_id']) && $user['_id'] === $userId) {
-                                if (!isset($user['reviews'])) {
-                                    $user['reviews'] = [];
-                                }
-                                $user['reviews'][] = [
-                                    '_id' => $review['_id'],
-                                    'funhub_store_id' => $funhubStoreId,
-                                    'rating' => $rating,
-                                    'categories' => $categories,
-                                    'created_at' => $createdAt ? date('Y-m-d H:i:s', strtotime($createdAt)) : null
-                                ];
+        //                 // Find the user by ID
+        //                 foreach ($uniqueUsers as &$user) {
+        //                     if (isset($user['_id']) && $user['_id'] === $userId) {
+        //                         if (!isset($user['reviews'])) {
+        //                             $user['reviews'] = [];
+        //                         }
+        //                         $user['reviews'][] = [
+        //                             '_id' => $review['_id'],
+        //                             'funhub_store_id' => $funhubStoreId,
+        //                             'rating' => $rating,
+        //                             'categories' => $categories,
+        //                             'created_at' => $createdAt ? date('Y-m-d H:i:s', strtotime($createdAt)) : null
+        //                         ];
 
-                                $this->info("-- Store Rating with external_review_id: " . $review['_id'] . " append to user: " . $user['User name']);
-                                break;
-                            }
-                        }
-                    }
-                } else {
-                    $this->error("Review missing Created By or Store: " . json_encode($review));
-                }
-            }
-        }
+        //                         $this->info("-- Store Rating with external_review_id: " . $review['_id'] . " append to user: " . $user['User name']);
+        //                         break;
+        //                     }
+        //                 }
+        //             }
+        //         } else {
+        //             $this->error("Review missing Created By or Store: " . json_encode($review));
+        //         }
+        //     }
+        // }
 
-        // Sort reviews for each user by latest first, then oldest
-        foreach ($uniqueUsers as &$user) {
-            $this->info("Sorting reviews for user: " . $user['_id']);
-            if (isset($user['reviews'])) {
-                usort($user['reviews'], function($a, $b) {
-                    return strtotime($b['created_at']) - strtotime($a['created_at']);
-                });
-            }
-        }
+        // // Sort reviews for each user by latest first, then oldest
+        // foreach ($uniqueUsers as &$user) {
+        //     $this->info("Sorting reviews for user: " . $user['_id']);
+        //     if (isset($user['reviews'])) {
+        //         usort($user['reviews'], function($a, $b) {
+        //             return strtotime($b['created_at']) - strtotime($a['created_at']);
+        //         });
+        //     }
+        // }
 
-        $this->info(var_dump($uniqueUsers));
+        // collect all results using laravel collection
+        $reviewsData = collect($reviewsData);
+        $usersData = collect($usersData);
+        $storesData = collect($storesData);
+
+        // reviewsData sort by Created Date
+        $reviewsData = $reviewsData->sortByDesc('Created Date');
 
         $availableRatingCategories = RatingCategory::all();
-        $reviewsData = collect($reviewsData);
+        foreach ($reviewsData as $review) {
+            //print new line
+            $this->info("--------------------------------------------------------------------------------");
+            $this->info("Process Review data: " . json_encode($review));
+            // get review user
+            $user = $usersData->where('_id', $review['User Name'])->first();
 
-        // Output the formatted data
-        foreach ($uniqueUsers as $phoneNumber => $user) {
-            // create user if not exists match by phone number
-            $authUser = User::where('phone_no', ltrim($user['Phone number'], '0'))
-                ->where('phone_country_code', '60') // only MY
-                ->first();
+            if ($user) {
+                // check if user exists in our database or not
+                $authUser = $this->getAuthUser($user);
+                if ($authUser) {
+                    // user exists in our database
+                    $this->info("User exists in our database: " . $user['User name']);
+                    $bubbleStore = $storesData->where('_id', $review['Store'])->first(); // buuble store data must have funhub_store_id
+                    if (!$bubbleStore) {
+                        $this->error("-- Store ID: " . $review['Store'] . " funhub_store_id not found");
+                        continue;
+                    }
 
-            if (!$authUser) {
-              try {
-                $authUser = User::create([
-                    'name' => $user['User name'],
-                    'phone_country_code' => '60',
-                    'phone_no' => $user['Phone number'],
-                ]);
+                    $store = Store::where('id', $bubbleStore['funhub_store_id'])->first();
+                    if (!$store) {
+                        $this->error("-- Store ID: " . $bubbleStore['funhub_store_id'] . " not found in database" . json_encode($review));
+                        continue;
+                    }
 
-                Log::info('[PullBubbleDataForUserStoreRatings] Bubble API User Created', [
-                    'user_id' => $authUser->id,
-                    'phone_no' => $user['Phone number'],
-                    'name' => $user['User name'],
-                ]);
-              } catch (\Exception $e) {
-                $this->error("Failed to create user: " . $user['User name']. ' phone_no: ' . $user['Phone number']);
-                continue;
-              }
-
-              $this->info("Created: " . $user['User name']. ' with id: ' . $authUser->id. '  phone_no: ' . $user['Phone number']);
-            } else {
-                $this->info("Found existing user: " . $user['User name']. ' with id: ' . $authUser->id. '  phone_no: ' . $user['Phone number']);
-            }
-
-            if (isset($user['reviews'])) {
-                foreach ($user['reviews'] as $review) {
-                    // find store by funhub_store_id
-                    $store = Store::where('id', $review['funhub_store_id'])->first();
                     $storeRating = StoreRating::where('external_review_id', $review['_id'])->exists();
 
-                    // if ($storeRating) {
-                    //     $this->info("-- Store Rating with external_review_id: " . $review['_id'] . " already exists");
-                    // }
+                    if ($storeRating) {
+                        $this->error("-- Store Rating with external_review_id: " . $review['_id'] . " already exists");
+                        continue;
+                    }
 
-                    if ($store && $authUser && !$storeRating) {
+                    if ($store && $authUser && !$storeRating) { // if store exists and authUser exists and storeRating not exists
                         // add store ratings of this user id
                         $rating = $store->storeRatings()->create([
                             'user_id' => $authUser->id,
-                            'rating' => $review['rating'],
-                            'comment' => (isset($review['comment'])) ? $review['comment'] : null,
+                            'rating' => $review['Rating'],
+                            'comment' => (isset($review['Comments'])) ? $review['Comments'] : null,
                             'external_review_id' => $review['_id'],
                         ]);
+                        $this->info("-- Created rating for store: " . $store->id . " and user: " . $authUser->id);
 
-                        $categories = $review['categories'];
+                        $categories = $review['Topic']; // topics are rating categories
                         // loop through each category thats available in availableRatingCategories and attach to rating
                         foreach ($categories as $category) {
                             if ($availableRatingCategories->contains('name', $category)) {
-                                $rating->ratingCategories()->attach($availableRatingCategories->where('name', $category)->first()->id, ['user_id' => $authUser->id]);
+                                $this->info("-- Attaching rating category: " . $category . " to review");
+                                $rating->ratingCategories()->attach($availableRatingCategories->where('name', $category)->first()->id,
+                                     ['user_id' => $authUser->id]
+                                );
                             }
                         }
-
-                        Log::info('[PullBubbleDataForUserStoreRatings] Bubble API Store Rating Created', [
-                            'user_id' => $authUser->id,
-                            'store_id' => $review['funhub_store_id'],
-                            'rating' => $review['rating'],
-                            'categories' => $categories,
-                        ]);
-
-                        $this->info("Added rating REVIEW ID: ". $review['_id']. ' Ratings: ' . $review['rating'] . " to user: " . $user['User name']);
-
-                    } else {
-                        $this->error("Store ID: " . $review['funhub_store_id'] . " not found OR created before OR authed user not created. Review:" . json_encode(
-                            [
-                                'user_id' => $user['_id'] ?? 'null',
-                                'store_id' => $review['funhub_store_id'] ?? 'null',
-                                'external_review_id' => $review['_id'] ?? 'null',
-                                'store' => ($store) ? true : false,
-                                'authUser' => ($authUser) ? true : false,
-                                'storeRating' => ($storeRating) ? true : false,
-                            ]
-                        ));
-
-                        Log::error('[PullBubbleDataForUserStoreRatings] Bubble API Store ID not found OR authed user not created',   [
-                            'user_id' => $user['_id'] ?? 'null',
-                            'store_id' => $review['funhub_store_id'] ?? 'null',
-                            'external_review_id' => $review['_id'] ?? 'null',
-                            'store' => ($store) ? true : false,
-                            'authUser' => ($authUser) ? true : false,
-                            'storeRating' => ($storeRating) ? true : false,
-                        ]);
                     }
                 }
             } else {
-                $this->info("No reviews found for user: " . $user['User name']);
+                // no user found from review data struct
+                $this->error("-- User not found from review data: " . json_encode($review));
             }
-        }
+        } // end of foreach reviewsData
 
-        $this->info('Bubble data sync completed successfully.');
         return Command::SUCCESS;
+    }
+
+    protected function getAuthUser($userData) {
+        if (!isset($userData['Phone number'])) {
+            $this->error("-- User data does not have phone number: " . json_encode($userData));
+            return null;
+        }
+        $authUser = User::where('phone_no', trim(ltrim($userData['Phone number'], '0')))
+            ->where('phone_country_code', '60') // only MY
+            ->first();
+
+        if (!$authUser) {
+            try {
+                $authUser = User::create([
+                    'name' => $userData['User name'],
+                    'phone_country_code' => '60',
+                    'phone_no' => $userData['Phone number'],
+                ]);
+
+                $this->info("Created: " . $userData['User name']. ' with id: ' . $authUser->id. '  phone_no: ' . $userData['Phone number']);
+
+                Log::info('[PullBubbleDataForUserStoreRatings] Bubble API User Created', [
+                    'user_id' => $authUser->id,
+                    'phone_no' => $userData['Phone number'],
+                    'name' => $userData['User name'],
+                ]);
+            } catch (\Exception $e) {
+                $this->error("Failed to create user: " . $userData['User name']. ' phone_no: ' . $userData['Phone number']);
+                return null;
+            }
+        } else {
+            $this->info("Found existing user: " . $userData['User name']. ' with id: ' . $authUser->id. '  phone_no: ' . $userData['Phone number']);
+        }
+        return $authUser;
     }
 
     protected function getUniqueUsersByPhoneNumber($usersData) {
