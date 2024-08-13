28e472888f78c971efcd9f44705e4757c9f3b06b
Elson Tan <elsodev.et@gmail.com>
2024-08-13 14:51:01 +0800

    TEST FIX

--- app/Http/Controllers/Api/MissionController.php
+++ app/Http/Controllers/Api/MissionController.php
@@ -88,13 +88,9 @@ class MissionController extends Controller
 
         // filter by claimed_only false
         $query->when($request->has('claimed_only') && $request->claimed_only == 0, function($query) {
-            $query->where(function ($query) {
-                $query->whereHas('participants', function($query) {
-                    $query->where('user_id', auth()->user()->id)
-                        ->whereNull('missions_users.claimed_at');
-                })->orWhereDoesntHave('participants', function($query) {
-                    $query->where('user_id', auth()->user()->id);
-                });
+            $query->whereHas('participants', function($query) {
+                $query->where('user_id', auth()->user()->id)
+                    ->whereNull('missions_users.claimed_at');
             });
         });
 
