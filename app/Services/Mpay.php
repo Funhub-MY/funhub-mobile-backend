c88ba083f40633f229531a56f29bc5da77d01fe0
Elson Tan <elsodev.et@gmail.com>
2024-09-05 21:36:52 +0800

    FIX get payment types availaber mpay account

--- app/Services/Mpay.php
+++ app/Services/Mpay.php
@@ -161,8 +161,16 @@ class Mpay {
 
         if ($response->successful()) {
             $responseData = $response->json();
-            if ($responseData['responseCode'] == '00') {
-                return $responseData['paymentTypeList'];
+            if ($responseData['responseCode'] == '00' && isset($responseData['paymentTypeList'])) {
+                // decode [Card, FPX-ABB0234, FPX-Affin Bank, FPX-AGRONet, FPX-Alliance Bank, FPX-AmBank, FPX-Bank Islam, FPX-Bank Muamalat, FPX-Bank Of China, FPX-Bank Rakyat, FPX-BSN, FPX-CIMB Clicks, FPX-Citibank, FPX-Hong Leong Bank, FPX-HSBC Bank, FPX-KFH, FPX-Maybank2U, FPX-OCBC Bank, FPX-Public Bank, FPX-RHB Bank, FPX-SBI Bank A, FPX-SBI Bank B, FPX-SBI Bank C, FPX-Standard Chartered, FPX-UOB Bank, FPX-UOB0229, FPX-B2B-Affin Bank, FPX-B2B-AFFINMAX, FPX-B2B-AGRONetBIZ, FPX-B2B-Alliance Bank, FPX-B2B-AmBank, FPX-B2B-Bank Islam, FPX-B2B-Bank Muamalat, FPX-B2B-Bank Rakyat, FPX-B2B-BNP Paribas, FPX-B2B-CIMB Bank, FPX-B2B-Citibank CorporateBanking, FPX-B2B-Deutsche Bank, FPX-B2B-Hong Leong Bank, FPX-B2B-HSBC Bank, FPX-B2B-KFH, FPX-B2B-LOAD001, FPX-B2B-Maybank2E, FPX-B2B-OCBC Bank, FPX-B2B-PBB0234, FPX-B2B-Public Bank, FPX-B2B-RHB Bank, FPX-B2B-SBI Bank A, FPX-B2B-SBI Bank B, FPX-B2B-SBI Bank C, FPX-B2B-Standard Chartered, FPX-B2B-UOB Bank, FPX-B2B-UOB0228, Boost, GrabPay, TNG]
+                // as array
+                $paymentTypes = explode(',', $responseData['paymentTypeList']);
+                // remove any prefix with [ or suffix ] and trim
+                $paymentTypes = array_map(function ($paymentType) {
+                    return trim(str_replace(['[', ']'], '', $paymentType));
+                }, $paymentTypes);
+
+                return $paymentTypes;
             } else {
                 Log::error('Error checking available payment types: ' . $responseData['responseDesc']);
                 return [];
@@ -175,7 +183,10 @@ class Mpay {
 
     private function generateHashForCheckPaymentType()
     {
-        $string = $this->hashKey . $this->mid;
+        $string = $this->hashKey . 'Continue' . $this->mid;
+        Log::info('Mpay generateHashForCheckPaymentType', [
+            'string' => $string,
+        ]);
         return $this->secureHash->generateSecureHash($string);
     }
 
