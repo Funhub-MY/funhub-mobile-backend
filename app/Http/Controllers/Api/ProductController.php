9edd415ce72bd506081a0d65769b0058b979bbe2
Elson Tan <elsodev.et@gmail.com>
2024-09-17 17:36:46 +0800

    FIX product selected card

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -123,11 +123,12 @@ class ProductController extends Controller
 
         // get card if payment mode via card
         $selectedCard = null;
-        if ($request->fiat_payment_method == 'card') {
+        if ($request->fiat_payment_method == 'card' && $request->has('card_id')) {
             // check user has a saved card
-            $selectedCard = UserCard::where('user_id', $user->id)
-                ->where('is_default', true)
-                ->notExpired()
+            $selectedCard = UserCard::where('id', $request->card_id)
+                ->where('user_id', $user->id)
+                // ->where('is_default', true)
+                // ->notExpired()
                 ->first();
 
             if (!$selectedCard) {
