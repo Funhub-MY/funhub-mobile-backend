610d5d5d27139ac6e9a76af95b2dde62ab924cb5
Steven Yew <yewsnet0174761163@gmail.com>
2024-09-25 13:05:40 +0800

    Missing comma in payment handler blade

--- resources/views/payment-return.blade.php
+++ resources/views/payment-return.blade.php
@@ -6,7 +6,7 @@
 <script>
     window.flutter_inappwebview.callHandler('paymentData', {
         'success': {{ $success }},
-        'transaction_id': "{{ $transaction_id }}"
+        'transaction_id': "{{ $transaction_id }}",
         'offer_claim_id': "{{ (isset($offer_claim_id)) ? $offer_claim_id : '' }}",
         'redemption_start_date': "{{ isset($redemption_start_date) ? $redemption_start_date : '' }}",
         'redemption_end_date': "{{ isset($redemption_end_date) ? $redemption_end_date : '' }}",
