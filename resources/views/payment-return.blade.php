fb6d6f93490a2603efb3e7f852b14ed2c2ce64d5
Steven Yew <yewsnet0174761163@gmail.com>
2024-09-27 12:56:49 +0800

    UPDATED

--- resources/views/payment-return.blade.php
+++ resources/views/payment-return.blade.php
@@ -6,13 +6,11 @@
 <script>
     console.log('Mpay Payment Return');
     window.flutter_inappwebview.callHandler('paymentData', {
-        'success': {{  $success ? 1 : 0 }},
+        'success': {{ $success ? 1 : 0 }},
         'transaction_id': "{{ $transaction_id }}",
-        /*
         'offer_claim_id': "{{ (isset($offer_claim_id)) ? $offer_claim_id : 'null' }}",
         'redemption_start_date': "{{ isset($redemption_start_date) ? $redemption_start_date : 'null' }}",
         'redemption_end_date': "{{ isset($redemption_end_date) ? $redemption_end_date : 'null' }}",
-        */
     });
 </script>
 <b>Mpay Payment Return</b>
