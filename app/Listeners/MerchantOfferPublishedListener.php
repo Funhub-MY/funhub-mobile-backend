16f9dbb85ffa9bddf38b0436d8ab341d85a1ff64
Elson Tan <elsodev.et@gmail.com>
2024-10-10 06:36:37 +0800

    ADHOC PROD ADDED more error catch for merchant offer publish

--- app/Listeners/MerchantOfferPublishedListener.php
+++ app/Listeners/MerchantOfferPublishedListener.php
@@ -31,32 +31,38 @@ class MerchantOfferPublishedListener
      */
     public function handle($event)
     {
-        $offer = $event->offer;
+       try {
+            $offer = $event->merchantOffer;
 
-        if ($offer->status != MerchantOffer::STATUS_PUBLISHED || get_class($offer) != MerchantOffer::class) {
-            Log::info('[MerchantOfferPublishedListener] Merchant offer not published or not MerchantOffer instance, skip');
-            return;
-        }
-
-        // fire new offer listed notification
-        // get all users who has added interaction bookmark to related merchant offer stores get notified
-        $merchantOfferStores = $offer->stores()->get();
-        $interactions = Interaction::where('interactable_type', Store::class)
-            ->whereIn('interactable_id', $merchantOfferStores->pluck('id'))
-            ->where('type', Interaction::TYPE_BOOKMARK)
-            ->whereHas('user', function ($query) {
-                $query->where('status', User::STATUS_ACTIVE);
-            })
-            ->get();
-
-        foreach ($interactions as $interaction) {
-            $user = $interaction->user;
-            try {
-                $locale = $user->last_lang ?? config('app.locale');
-                $user->notify((new NewMerchantOfferListed($offer))->locale($locale));
-            } catch (\Exception $e) {
-                Log::error('[PublishMerchantOffer] Notification error when bookmarked merchant offer published: '.$offer->id.' to user: '.$user->id);
+            if ($offer->status != MerchantOffer::STATUS_PUBLISHED || get_class($offer) != MerchantOffer::class) {
+                Log::info('[MerchantOfferPublishedListener] Merchant offer not published or not MerchantOffer instance, skip');
+                return;
             }
-        }
+
+            // fire new offer listed notification
+            // get all users who has added interaction bookmark to related merchant offer stores get notified
+            $merchantOfferStores = $offer->stores()->get();
+            $interactions = Interaction::where('interactable_type', Store::class)
+                ->whereIn('interactable_id', $merchantOfferStores->pluck('id'))
+                ->where('type', Interaction::TYPE_BOOKMARK)
+                ->whereHas('user', function ($query) {
+                    $query->where('status', User::STATUS_ACTIVE);
+                })
+                ->get();
+
+            foreach ($interactions as $interaction) {
+                $user = $interaction->user;
+                try {
+                    $locale = $user->last_lang ?? config('app.locale');
+                    $user->notify((new NewMerchantOfferListed($offer))->locale($locale));
+                } catch (\Exception $e) {
+                    Log::error('[PublishMerchantOffer] Notification error when bookmarked merchant offer published: '.$offer->id.' to user: '.$user->id);
+                }
+            }
+       } catch (\Exception $e) {
+           Log::error('[MerchantOfferPublishedListener] Error when bookmarked merchant offer published', [
+               'error' => $e->getMessage(),
+           ]);
+       }
     }
 }
