46a811bcf80a15a0a20b364268016379ec79aee4
Elson Tan <elsodev.et@gmail.com>
2024-11-04 11:57:12 +0800

    FIX: outlet in mall location issue

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -1018,7 +1018,7 @@ class ArticleController extends Controller
 
         // Mall outlets incorrect attaching issue
         // if location exists, check if is_mall, if is mall check if name of locationData same as location name
-        if ($location->is_mall && $locationData['name'] != $location->name) {
+        if ($location && $location->is_mall && $locationData['name'] != $location->name) {
             // eg. location name is Chagee @ Sunway Pyramid it will have same lat,lng and google_id as Sunway Pyramid
             // to prevent Chagee @ Sunway Pyramid being attached incorrectly to Sunway Pyramid
             // search again with name of locationData, lat, lng. instead of just google_id or lat/lng
