ea9719218e78b3eac9541c25c83ed59ede9d5fc8
Elson Tan <elsodev.et@gmail.com>
2024-11-15 10:47:36 +0800

    ADDED identify merchant offers exists for an article

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -1754,6 +1754,31 @@ class ArticleController extends Controller
 
         // pass to query builder
         $data = $this->articleQueryBuilder($query, $request, false);
+
+        $locationIds = $data->pluck('location.0.id')->filter()->unique()->toArray();
+
+        $storesWithOffers = DB::table('locatables as store_locatables')
+            ->whereIn('store_locatables.location_id', $locationIds)
+            ->where('store_locatables.locatable_type', Store::class)
+            ->join('merchant_offer_stores', 'store_locatables.locatable_id', '=', 'merchant_offer_stores.store_id')
+            ->join('merchant_offers', function ($join) {
+                $join->on('merchant_offer_stores.merchant_offer_id', '=', 'merchant_offers.id')
+                    ->where('merchant_offers.status', '=', MerchantOffer::STATUS_PUBLISHED)
+                    ->where('merchant_offers.available_at', '<=', now())
+                    ->where('merchant_offers.available_until', '>=', now());
+            })
+            ->pluck('store_locatables.location_id')
+            ->unique();
+
+        $data->each(function ($article) use ($storesWithOffers) {
+            if ($article->location->isNotEmpty()) {
+                $articleLocationId = $article->location->first()->id;
+                $article->has_merchant_offer = $storesWithOffers->contains($articleLocationId);
+            } else {
+                $article->has_merchant_offer = false;
+            }
+        });
+
         $data = $query->paginate($request->has('limit') ? $request->limit : config('app.paginate_per_page'));
 
         return PublicArticleResource::collection($data);
