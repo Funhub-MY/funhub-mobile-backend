b559648e0893b78cbdd1febd72426e6ccb99afe9
Elson Tan <elsodev.et@gmail.com>
2025-02-09 20:53:57 +0800

    ADDED ->onOneServer(); to commands to prevent duplicate runs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,57 +16,56 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // Every Minute
-        $schedule->command('send-custom-notification')->everyMinute();
-        $schedule->command('articles:run-engagements')->everyMinute();
+        $schedule->command('send-custom-notification')->everyMinute()->onOneServer();
+        $schedule->command('articles:run-engagements')->everyMinute()->onOneServer();
 
         // Every Five Minutes
-		$schedule->command('article:publish')->everyFiveMinutes();
+		$schedule->command('article:publish')->everyFiveMinutes()->onOneServer();
 
         if (config('services.byteplus.enabled_vod') == true) {
-            $schedule->command('byteplus:check-video-status')->everyFiveMinutes()->withoutOverlapping();
+            $schedule->command('byteplus:check-video-status')->everyFiveMinutes()->withoutOverlapping()->onOneServer();
         }
 
 		// Every Ten Minutes
-		$schedule->command('articles:sync-location-as-stores')->everyTenMinutes();
+		$schedule->command('articles:sync-location-as-stores')->everyTenMinutes()->onOneServer();
 
         // Every Fifteen Minutes
-        $schedule->command('merchant-offers:release')->everyFifteenMinutes();
-        $schedule->command('articles:sync-ratings-to-store-ratings')->everyFifteenMinutes()->withoutOverlapping();
+        $schedule->command('merchant-offers:release')->everyFifteenMinutes()->onOneServer();
+        $schedule->command('articles:sync-ratings-to-store-ratings')->everyFifteenMinutes()->withoutOverlapping()->onOneServer();
         if (config('services.bubble.status') == true) {
-            $schedule->command('bubble:sync-user-store-ratings')->everyFifteenMinutes();
+            $schedule->command('bubble:sync-user-store-ratings')->everyFifteenMinutes()->onOneServer();
         }
 
 		// Every Thirty Minutes
-		$schedule->command('city-names:populate')->everyThirtyMinutes();
+		$schedule->command('city-names:populate')->everyThirtyMinutes()->onOneServer();
         if (config('app.auto_article_categories') == true) {
-            $schedule->command('articles:categorize')->everyThirtyMinutes();
+            $schedule->command('articles:categorize')->everyThirtyMinutes()->onOneServer();
         }
 
         // Hourly
-        $schedule->command('fetch:news-feed')->hourly();
-        $schedule->command('media-partner:auto-publish-by-keywords')->hourly();
-        $schedule->job(new \App\Jobs\ImportedContactMatching())->hourly();
-        $schedule->command('stores:auto-hide-unonboarded')->hourly();
-		$schedule->command('redeem:send-review-reminder')->hourly();
+        $schedule->command('fetch:news-feed')->hourly()->onOneServer();
+        $schedule->command('media-partner:auto-publish-by-keywords')->hourly()->onOneServer();
+        $schedule->job(new \App\Jobs\ImportedContactMatching())->hourly()->onOneServer();
+        $schedule->command('stores:auto-hide-unonboarded')->hourly()->onOneServer();
+		$schedule->command('redeem:send-review-reminder')->hourly()->onOneServer();
 
         // Daily at Specific Times
-        $schedule->command('merchant-offers:publish')->dailyAt('00:00')->withoutOverlapping(10);
-        $schedule->command('merchant-offers:auto-archieve')->dailyAt('00:01');
-        $schedule->command('article:auto-archive')->dailyAt('00:05');
-        $schedule->command('merchant-offers:send-expiring-notification')->dailyAt('00:10');
-        $schedule->command('merchant-offers:auto-move-vouchers-unsold')->dailyAt('00:15');
+        $schedule->command('merchant-offers:publish')->dailyAt('00:00')->withoutOverlapping(10)->onOneServer();
+        $schedule->command('merchant-offers:auto-archieve')->dailyAt('00:01')->onOneServer();
+        $schedule->command('article:auto-archive')->dailyAt('00:05')->onOneServer();
+        $schedule->command('merchant-offers:send-expiring-notification')->dailyAt('00:10')->onOneServer();
+        $schedule->command('merchant-offers:auto-move-vouchers-unsold')->dailyAt('00:15')->withoutOverlapping(10)->onOneServer();
         if (config('app.auto_redistribute_vouchers') == true) {
-            $schedule->command('campaign:redistribute-quantities')->dailyAt('00:30');
+            $schedule->command('campaign:redistribute-quantities')->dailyAt('00:30')->onOneServer();
         }
-		$schedule->command('articles:check-expired')->dailyAt('01:00');
+		$schedule->command('articles:check-expired')->dailyAt('01:00')->onOneServer();
 
         // Every Two Hours
-        $schedule->command('generate:article-views')->everyTwoHours();
-        $schedule->command('update:scheduled-views')->everyTwoHours();
+        $schedule->command('generate:article-views')->everyTwoHours()->onOneServer();
+        $schedule->command('update:scheduled-views')->everyTwoHours()->onOneServer();
 
         // Daily
-        $schedule->command('telescope:prune')->daily();
-
+        $schedule->command('telescope:prune')->daily()->onOneServer();
     }
 
     /**
