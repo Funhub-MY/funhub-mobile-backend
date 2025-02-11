150378113b36f95a9d9069834884f4d22e71fd87
Elson Tan <elsodev.et@gmail.com>
2025-02-11 20:57:43 +0800

    ADHOC PREPROD test ip

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,7 @@ Route::get('/auth/facebook/callback', function () {
 // this call back is use for socialite callback only for unit test. not using at any place.
 Route::get('/auth/google/callback', [\App\Http\Controllers\Api\AuthController::class, 'googleCallBack']);
 
+
 Route::get('/merchants/register', MerchantRegister::class)->name('merchant.register');
 
 /*
@@ -84,3 +85,28 @@ Route::get('/test-email', function () {
     ]);
 });
 */
+
+
+// test route for IP and headers information
+Route::get('/test-1/ip-info', function (\Illuminate\Http\Request $request) {
+    return response()->json([
+        'client_ip' => $request->ip(),
+        'real_ip' => $request->getClientIp(),
+        'all_ips' => $request->ips(),
+        'x_forwarded_for' => $request->header('X-Forwarded-For'),
+        'x_real_ip' => $request->header('X-Real-IP'),
+        'server_addr' => $_SERVER['SERVER_ADDR'] ?? null,
+        'remote_addr' => $_SERVER['REMOTE_ADDR'] ?? null,
+        'all_headers' => $request->headers->all(),
+        'server_vars' => array_intersect_key($_SERVER, array_flip([
+            'HTTP_X_FORWARDED_FOR',
+            'HTTP_X_FORWARDED_PROTO',
+            'HTTP_X_FORWARDED_PORT',
+            'HTTP_X_REAL_IP',
+            'REMOTE_ADDR',
+            'SERVER_ADDR',
+            'SERVER_PORT',
+            'SERVER_PROTOCOL'
+        ]))
+    ]);
+});
