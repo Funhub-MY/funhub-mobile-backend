461bb8c8b74c27d323bd11d36466be1663bc39ad
benny.lim <benny.lim@funhub.my>
2026-05-18 18:20:00 +0800

    Promo code update

--- app/Filament/Resources/PromotionCodeGroupResource/Pages/CreatePromotionCodeGroup.php
+++ app/Filament/Resources/PromotionCodeGroupResource/Pages/CreatePromotionCodeGroup.php
@@ -4,15 +4,12 @@ namespace App\Filament\Resources\PromotionCodeGroupResource\Pages;
 
 use App\Filament\Resources\PromotionCodeGroupResource;
 use App\Jobs\GeneratePromotionCodesJob;
-use App\Models\PromotionCode;
 use App\Models\PromotionCodeGroup;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use App\Models\Reward;
-use App\Models\RewardComponent;
-use Filament\Notifications\Notification;
 
 class CreatePromotionCodeGroup extends CreateRecord
 {
@@ -23,58 +20,47 @@ class CreatePromotionCodeGroup extends CreateRecord
         Log::info('[PromotionCodeGroup] Creating promotion code group', $data);
 
         return DB::transaction(function () use ($data) {
-
-            if ($data['code_type'] == PromotionCodeGroup::CODE_TYPES['random']) {
-                $totalCodes = $data['total_codes'];
-            }
-           else {
-                $totalCodes = 1;
-           }
-
-            // Initialize variables for reward data
-            $rewardable_type = null;
-            $rewardable_id = null;
-            $quantity = null;
-
-            // Only process reward data if not using fix amount discount
-            if (!($data['discount_type'] === 'fix_amount')) {
-                // rewards data
-                $rewardable_type = $data['rewardable_type'] ?? null;
-                $rewardable_id = $data['rewardable_id'] ?? null;
-                $quantity = $data['quantity'] ?? null;
-
-                // remove fields that don't belong in PromotionCodeGroup
-                unset($data['rewardable_type']);
-                unset($data['rewardable_id']);
-                unset($data['quantity']);
-            }
-
-            // Create the promotion code group first
-            $group = static::getModel()::create($data);
-
-            // Prepare job data
             $jobData = $data;
 
-            // Add back reward data if it was removed
-            if (!($data['use_fix_amount_discount'] ?? false)) {
-                $jobData['rewardable_type'] = $rewardable_type;
-                $jobData['rewardable_id'] = $rewardable_id;
-                $jobData['quantity'] = $quantity;
+            $totalCodes = ($data['code_type'] ?? null) === 'random'
+                ? (int) ($data['total_codes'] ?? 1)
+                : 1;
+
+            $products = $data['products'] ?? [];
+            $paymentMethods = $data['paymentMethods'] ?? [];
+
+            $groupAttributes = collect($data)->except([
+                'static_code',
+                'products',
+                'paymentMethods',
+                'rewardable_type',
+                'rewardable_id',
+                'quantity',
+            ])->toArray();
+
+            $groupAttributes['use_fix_amount_discount'] = ($groupAttributes['discount_type'] ?? null) === 'fix_amount';
+
+            $group = static::getModel()::create($groupAttributes);
+
+            if (! empty($products)) {
+                $group->products()->sync($products);
             }
 
+            if (! empty($paymentMethods)) {
+                $group->paymentMethods()->sync($paymentMethods);
+            }
 
-            // Dispatch the job to generate promotion codes
-            GeneratePromotionCodesJob::dispatch($group, $jobData);
+            GeneratePromotionCodesJob::dispatchSync($group, $jobData);
 
             Notification::make()
                 ->title('Promotion Code Group Created')
-                ->body("The promotion code group has been created. {$totalCodes} codes are being generated in the background.")
+                ->body("The promotion code group has been created with {$totalCodes} code(s).")
                 ->success()
                 ->send();
 
-            Log::info('[PromotionCodeGroup] Dispatched job to generate promotion codes', [
+            Log::info('[PromotionCodeGroup] Generated promotion codes', [
                 'promotion_code_group_id' => $group->id,
-                'total_codes' => $totalCodes
+                'total_codes' => $totalCodes,
             ]);
 
             return $group;
