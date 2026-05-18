02f5011e4fb3c856b630662902a3b24d2bdd8167
benny.lim <benny.lim@funhub.my>
2026-05-18 18:08:45 +0800

    Promo code editable

--- app/Filament/Resources/PromotionCodeGroupResource/Pages/EditPromotionCodeGroup.php
+++ app/Filament/Resources/PromotionCodeGroupResource/Pages/EditPromotionCodeGroup.php
@@ -10,10 +10,13 @@ use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Notifications\Notification;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class EditPromotionCodeGroup extends EditRecord
 {
     protected static string $resource = PromotionCodeGroupResource::class;
+
+    protected ?string $pendingStaticCode = null;
 
     protected function getActions(): array
     {
@@ -30,7 +33,7 @@ class EditPromotionCodeGroup extends EditRecord
             ->first();
 
 		if ($this->record->code_type == 'static') {
-			$data['static_code'] = $promotionCode->code ?? '';
+			$data['static_code'] = $promotionCode?->code ?? '';
 		}
 
         if ($promotionCode) {
@@ -48,8 +51,38 @@ class EditPromotionCodeGroup extends EditRecord
         return $data;
     }
 
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (array_key_exists('static_code', $data)) {
+            $this->pendingStaticCode = $data['static_code'];
+            unset($data['static_code']);
+
+            if ($this->record->hasUserRedemptions()) {
+                $currentCode = PromotionCode::where('promotion_code_group_id', $this->record->id)
+                    ->value('code');
+
+                if ($currentCode && strtoupper($this->pendingStaticCode) !== strtoupper($currentCode)) {
+                    throw ValidationException::withMessages([
+                        'static_code' => 'This promo code cannot be changed because it has already been redeemed by users.',
+                    ]);
+                }
+            }
+        }
+
+        return $data;
+    }
+
     protected function afterSave(): void
     {
+        if (
+            $this->record->code_type === 'static'
+            && filled($this->pendingStaticCode)
+            && ! $this->record->hasUserRedemptions()
+        ) {
+            PromotionCode::where('promotion_code_group_id', $this->record->id)
+                ->update(['code' => strtoupper($this->pendingStaticCode)]);
+        }
+
         // check if status was changed
         if ($this->record->wasChanged('status')) {
             // update all promotion codes under this group
